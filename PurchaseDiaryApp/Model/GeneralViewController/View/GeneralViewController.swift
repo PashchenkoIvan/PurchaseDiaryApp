20272aab6a4ef017cb416ea6a1a0f6483eda0f94
PashchenkoIvan <ipaschenko09@gmail.com>
2024-08-06 15:09:54 +0300

    Created MVC in project, created general, search, history, views

---
+++ PurchaseDiaryApp/Model/GeneralViewController/View/GeneralViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PurchaseDiaryApp
+//
+//  Created by Пащенко Иван on 06.08.2024.
+//
+
+import UIKit
+
+class GeneralViewController: UIViewController {
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var totalSpentLabel: UILabel!
+    @IBOutlet weak var totalCountLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
