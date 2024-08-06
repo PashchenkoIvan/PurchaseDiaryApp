66895fac5cd7c7bf5f5ae8d4fe457a9360311985
PashchenkoIvan <ipaschenko09@gmail.com>
2024-08-06 13:20:53 +0300

    Initial Commit

---
+++ PurchaseDiaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PurchaseDiaryApp
+//
+//  Created by Пащенко Иван on 06.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
