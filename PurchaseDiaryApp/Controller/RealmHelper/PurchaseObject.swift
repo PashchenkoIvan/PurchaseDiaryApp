cd5e900ed98c7b9169c5b5e8698a96e8d22def36
PashchenkoIvan <ipaschenko09@gmail.com>
2024-08-08 18:39:19 +0300

    Modify Realm helper

--- PurchaseDiaryApp/Controller/RealmHelper/PurchaseObject.swift
+++ PurchaseDiaryApp/Controller/RealmHelper/PurchaseObject.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 import RealmSwift
 
 class PurchaseObject: Object {
@@ -13,7 +14,7 @@ class PurchaseObject: Object {
     @Persisted var text: String
     @Persisted var cost: Double
     @Persisted var timestamp: Date
-    @Persisted var category: String
+    @Persisted var category: Int16
     @Persisted var latitude: Double // Широта
     @Persisted var longitude: Double // Долгота
     @Persisted var purchaseDescription: String? // Описание (опциональное)
@@ -27,12 +28,13 @@ extension PurchaseObject {
 }
 
 extension PurchaseObject {
-    static func addPurchase(text: String? = nil, cost: Double = 0.0, latitude: Double? = nil, longitude: Double? = nil, description: String? = "Not given") {
+    static func addPurchase(text: String? = nil, cost: Double = 0.0, category: Int16, latitude: Double? = nil, longitude: Double? = nil, description: String? = "Not given") {
         let newPurchase = PurchaseObject()
 
         newPurchase.text = text ?? "Not given"
         newPurchase.cost = cost
         newPurchase.timestamp = Date()
+        newPurchase.category = category
         newPurchase.latitude = latitude ?? 0.0
         newPurchase.longitude = longitude ?? 0.0
         newPurchase.purchaseDescription = description ?? "Not given"
@@ -42,6 +44,21 @@ extension PurchaseObject {
         try! realm.write {
             realm.add(newPurchase)
         }
+    }
+}
+
+extension PurchaseObject {
+    static func getPurchase(withId id: ObjectId) -> PurchaseObject? {
+        let realm = try! Realm()
+        return realm.object(ofType: PurchaseObject.self, forPrimaryKey: id)
+    }
+}
+
+extension PurchaseObject {
+    static func getCategoryPurchases(withCategory category: Int16) -> [PurchaseObject] {
+        let realm = try! Realm()
+        let results = realm.objects(PurchaseObject.self).filter("category == %@", category)
+        return Array(results)
     }
 }
 
---
+++ PurchaseDiaryApp/Controller/RealmHelper/Structs/CategoryInfo.swift
@@ -0,0 +1,33 @@
+//
+//  Category.swift
+//  PurchaseDiaryApp
+//
+//  Created by Пащенко Иван on 08.08.2024.
+//
+
+import Foundation
+import UIKit
+
+struct CategoryInfo {
+    let id: Int16
+    let name: String
+    let image: UIImage
+}
+
+let categoryArray: [CategoryInfo] = [
+    CategoryInfo.init(id: 1, name: "Entertainment", image: UIImage(systemName: "popcorn.circle.fill")!),
+    CategoryInfo.init(id: 2, name: "Cafes and Restaurants", image: UIImage(systemName: "fleuron.fill")!),
+    CategoryInfo.init(id: 3, name: "Auto", image: UIImage(systemName: "car.circle.fill")!),
+    CategoryInfo.init(id: 4, name: "Home Expenses", image: UIImage(systemName: "house.circle.fill")!),
+    CategoryInfo.init(id: 5, name: "Personal Expenses", image: UIImage(systemName: "figure.wave.circle.fill")!),
+    CategoryInfo.init(id: 6, name: "Health", image: UIImage(systemName: "stethoscope.circle.fill")!),
+    CategoryInfo.init(id: 7, name: "Travel", image: UIImage(systemName: "airplane.circle.fill")!),
+    CategoryInfo.init(id: 8, name: "Education", image: UIImage(systemName: "book.circle.fill")!),
+    CategoryInfo.init(id: 9, name: "Children", image: UIImage(systemName: "igure.and.child.holdinghands")!),
+    CategoryInfo.init(id: 10, name: "Pets", image: UIImage(systemName: "dog.circle.fill")!),
+    CategoryInfo.init(id: 11, name: "Electronics", image: UIImage(systemName: "visionpro.circle.fill")!),
+    CategoryInfo.init(id: 12, name: "Debts and Loans", image: UIImage(systemName: "creditcard.circle.fill")!),
+    CategoryInfo.init(id: 13, name: "Kids' Entertainment", image: UIImage(systemName: "figure.2.and.child.holdinghands")!),
+    CategoryInfo.init(id: 14, name: "Garden", image: UIImage(systemName: "tree.circle.fill")!),
+    CategoryInfo.init(id: 15, name: "Charity", image: UIImage(systemName: "giftcard.fill")!)
+]
